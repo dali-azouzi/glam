327dc93d481264e48b5ae46c02098f44c729c1f8
dali_azouzi <azouzidaliii@gmail.com>
2019-05-14 14:28:38 +0200

    api pulls

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,4 +56,16 @@ $(document).ready(function() {
 
 
 
+$.getJSON( "https://jsonp.afeld.me/?url=https%3A%2F%2Fapi.myglamapp.pl%2Fapi%2Fcategories%3Flanguage%3DEN", function(apiData){
 
+  for (let i = 0; i < apiData.data.length; i++) {
+    if( i==0){
+      document.getElementById('cardInject').innerHTML+='<div class="carousel-item col-md-4 active"> <div class="card"> <img class="card-img-top" src="http://'+apiData.data[i].imagePath+'" alt="Card image cap"> <div class="card-body"> <hr class="hrCard"> <p class="card-text text-center font-weight-bold">'+apiData.data[i].label+'</p> <p class=" text-center font-weight-light remove-margin">'+apiData.data[i].description+'</p></div></div>';
+
+    }
+    document.getElementById('cardInject').innerHTML+='<div class="carousel-item col-md-4"> <div class="card"> <img class="card-img-top" src="http://'+apiData.data[i].imagePath+'" alt="Card image cap"> <div class="card-body"> <hr class="hrCard"> <p class="card-text text-center font-weight-bold">'+apiData.data[i].label+'</p> <p class=" text-center font-weight-light remove-margin">'+apiData.data[i].description+'</p></div></div>';
+
+  
+  }
+
+})
